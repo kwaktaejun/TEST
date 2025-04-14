1f74747b27f6531d684fbe1beadd8069df1f0752
kwaktaejun <xowns900@gmail.com>
2025-04-14 20:57:23 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file
+import pandas as pd
 import os
+import io
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return 'Hello from Flask!'
+    return "Hello from Flask!"
 
-@app.route('/upload_excel_notion', methods=['GET', 'POST'])
-def upload_excel_notion():
-    if request.method == 'POST':
-        # 파일 처리 로직은 생략
-        return '업로드 완료'
-    return render_template('upload_excel_notion.html')
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
